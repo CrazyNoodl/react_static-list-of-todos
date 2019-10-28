da97067692a5bd6aab338cce0e3d1f9117e0ef54
Rudenko Artem <artem.rud@bk.ru>
2019-10-28 17:24:00 +0200

    Updates

--- precache-manifest.74314a75c355d0ceedae7b079eace40b.js
+++ precache-manifest.74314a75c355d0ceedae7b079eace40b.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f4c0541055d1cb80dc84d525e26cd92",
+    "revision": "7e7660a4ce4d578b5761e08300628b0c",
     "url": "/react_static-list-of-todos/index.html"
   },
   {
-    "revision": "b40e9ca27fda17825a70",
+    "revision": "8402a7430cb78c05dcf5",
     "url": "/react_static-list-of-todos/static/css/main.cb9e985a.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_static-list-of-todos/static/js/2.3310f33a.chunk.js"
+    "revision": "c886b51b1f95be8c5db7",
+    "url": "/react_static-list-of-todos/static/js/2.70263280.chunk.js"
   },
   {
-    "revision": "b40e9ca27fda17825a70",
-    "url": "/react_static-list-of-todos/static/js/main.1d2983b2.chunk.js"
+    "revision": "8402a7430cb78c05dcf5",
+    "url": "/react_static-list-of-todos/static/js/main.e2a4dfb7.chunk.js"
   },
   {
     "revision": "ea6f31786c2aed313835",
